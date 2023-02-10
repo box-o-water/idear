b9f52361375c00a9cc179a676c687ce6005a3783
box-o-water <boxooowater@gmail.com>
2023-02-09 20:59:47 -0600

    feat(homepage): got name and date working, a tiny bit of styling

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
-const { Idea } = require('../models');
+const { Idea, User } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
     // Get all ideas and JOIN with user data
     const data = await Idea.findAll({
-      // include: [
-      //   {
-      //     model: User,
-      //     attributes: ["username"],
-      //   },
-      // ],
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
     });
 
     // Serialize data so the template can read it
@@ -19,6 +19,28 @@ router.get('/', async (req, res) => {
     // Pass serialized data and session flag into template
     res.render('homepage', {
       ideas,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/idea/:id', async (req, res) => {
+  try {
+    const data = await Idea.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const idea = data.get({ plain: true });
+
+    res.render('idea', {
+      ...idea,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -10,8 +12,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
@@ -24,8 +25,8 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
