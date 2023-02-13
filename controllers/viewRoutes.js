152d86d2a0bfcd304bd1d0d2a1effde36e47837e
box-o-water <boxooowater@gmail.com>
2023-02-12 18:03:59 -0600

    feat(profile): add profile view, hook profile up with login and logout

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Idea, User, Choice } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
@@ -35,7 +36,7 @@ router.get('/idea/:id', async (req, res) => {
         },
         {
           model: Choice,
-        }
+        },
       ],
     });
 
@@ -44,6 +45,26 @@ router.get('/idea/:id', async (req, res) => {
     res.render('idea', {
       ...idea,
       logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Idea }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
